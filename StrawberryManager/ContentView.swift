942fb37a363bc31e920c68adaf5cb3d1f84d945c
TienNguyen <tiennguyen25081996@gmail.com>
2023-08-08 09:42:50 +0700

    test push code from terminal

--- StrawberryManager/ContentView.swift
+++ StrawberryManager/ContentView.swift
@@ -31,7 +31,6 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 extension ContentView {
-    
     var mainInterfaceView: some View{
         ZStack(alignment: .topLeading){
             MainTabView()
